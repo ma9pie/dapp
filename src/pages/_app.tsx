b70e9475e50d17b3182e3dca48c16471d5a6915b
ma9pie <ma9pie.dev@gmail.com>
2023-12-09 23:01:34 +0900

    fix : darkmode에서 테마 변경이 안되는 현상 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '@/styles/globals.css';
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import tw, { styled } from 'twin.macro';
 
 import Web3Modal from '@/components/modals/Web3Modal';
 
@@ -21,5 +24,21 @@ const App = (props: AppProps) => {
 export default App;
 
 const Inner = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps}></Component>;
+  const { theme } = useTheme();
+
+  const [className, setClassName] = useState<string | undefined>();
+
+  useEffect(() => {
+    setClassName(theme);
+  }, [theme]);
+
+  return (
+    <Wrapper className={className}>
+      <Component {...pageProps}></Component>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  ${tw`text-foreground bg-background`};
+`;
