350e0ce984f780c3cf80aa9a97edcc17206f27c7
ma9pie <ma9pie.dev@gmail.com>
2023-12-11 15:09:04 +0900

    chore : ellipsis default head tail 수정

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,8 +3,8 @@
  */
 export const ellipsis = (
   address: string | null,
-  head: number = 6,
-  tail: number = 6
+  head: number = 5,
+  tail: number = 5
 ) => {
   if (!address || head < 0 || tail < 0) return '';
   if (head === 0 && tail === 0) return address;
